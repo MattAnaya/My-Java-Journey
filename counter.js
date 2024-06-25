7796c1c3751271b00f645d5f128f30161bc1d4c0
Matt Anaya <150735056+MattAnaya@users.noreply.github.com>
2024-06-25 16:21:43 -0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -3,3 +3,15 @@ while(numeral <= 10){
     console.log(`Tu cuenta va en: ${numeral}`);
     numeral++;
 }
+
+/*
+Below you'll find a similar exercise, instead of adding 1 to the "numeral" we will decrement it by 1
+
+let counter = 10;
+while(counter >= 0){
+    console.log(`Tu secuencia va en: ${counter}`);
+    counter--;
+}
+
+If you analyze the main change will be the operator "--" that reduces the number by 1.
+*/
