39960ba6ad190fbc72a90549fb61cbdd35d2246d
Matt Anaya <150735056+MattAnaya@users.noreply.github.com>
2024-06-25 08:13:44 -0500

    Add files via upload

---
+++ saldo.js
@@ -0,0 +1,6 @@
+alert("Bienvenido a su Banco");
+let balance = 500;
+let saldo = prompt(`Ingresa la palabra "saldo" si quiere revisar su balance: `);
+if(saldo == 'Saldo'){
+    alert(`Tu saldo es ${balance}.`);
+}
