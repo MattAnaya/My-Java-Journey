27f33b32189ba5f37b17ea2857bc65c388b6bda2
Matt Anaya <150735056+MattAnaya@users.noreply.github.com>
2024-06-25 08:06:02 -0500

    Add files via upload
    Pracitcing and understanding how to declare a variable with "let" as well as utilizing 'if' 'else'  for different scenarios.

---
+++ semana.js
@@ -0,0 +1,6 @@
+let diaDeLaSemana = prompt("Que dia de la semana es?");
+if (diaDeLaSemana === 'Sabado' || diaDeLaSemana === 'Domingo') {
+    alert("Feliz fin de semana");
+} else {
+    alert("Feliz semana laboral");
+}
